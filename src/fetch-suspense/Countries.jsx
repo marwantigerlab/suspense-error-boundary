aebb1a262974a32c80b822fa08b8a4e9864b325e
marwantigerlab <marwan.mostafa@tigerlab.com>
2024-05-30 22:35:34 +0800

    error boundary reset behavior for time

--- src/fetch-suspense/Countries.jsx
+++ src/fetch-suspense/Countries.jsx
@@ -6,7 +6,6 @@ import Time from "./Time";
 
 // wrapping with suspense basically means, dont block while resolving
 
-
 // Todos:
 
 // to look into error boundary triggering a re-render of failed component
@@ -14,29 +13,37 @@ import Time from "./Time";
 // what happens if we wrap the ones that takes less time
 // useDeferredValue gets re-computed when all queued renders are flushed
 
-const baseurl = 'http://worldtimeapi.org/api/timezone/Asia'
+const baseurl = "http://worldtimeapi.org/api/timezone/Asia";
 
 const Countries = () => {
-    const [url, setUrl] = useState('http://worldtimeapi.org/api/timezone/Asia/Kolkata')
-    const [city, setCity] = useState('Kolkata')
-    const deferredCity= useDeferredValue(city)
+  const [url, setUrl] = useState(
+    "http://worldtimeapi.org/api/timezone/Asia/Kolkata"
+  );
+  const [city, setCity] = useState("Kolkata");
+  const deferredCity = useDeferredValue(city);
 
-    const handleChange = (e) => {
-        setCity(e.target.value)
-    }
+  const handleChange = (e) => {
+    setCity(e.target.value);
+  };
 
-    useEffect(()=>{
-        setUrl(`${baseurl}/${deferredCity}`)
-    },[deferredCity])
+  useEffect(() => {
+    setUrl(`${baseurl}/${deferredCity}`);
+  }, [deferredCity]);
 
   return (
     <>
       <h2>Countries with Time - Suspense & Error Boundaries</h2>
 
-     <input placeholder="city name" onChange={handleChange} />
+      <input placeholder="city name" onChange={handleChange} />
 
-       {/* 404 */}
-      <ErrorBoundary  fallback={<p>Something went wrong in fetching time</p>}>
+      {/* 404 */}
+      <ErrorBoundary
+        fallback={<p>Something went wrong in fetching time</p>}
+        onReset={() => {
+          setCity("Kolkata");
+        }}
+        resetKeys={[deferredCity]}
+      >
         <Suspense fallback={<p>Loading time...</p>}>
           <Time url={url} />
         </Suspense>
